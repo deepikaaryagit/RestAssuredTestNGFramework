f060a1ae1bb1ddd6f0ac9b8c3ed93d97d87eeea0
Arya <deepika.arya@capgemini.com>
2021-08-17 11:11:04 -0700

    status code added

--- src/test/java/com/spotify/oauth2/api/StatusCode.java
+++ src/test/java/com/spotify/oauth2/api/StatusCode.java
@@ -5,7 +5,8 @@ public enum StatusCode {
 	CODE_200(200,""),
 	CODE_201(201,""),
 	CODE_400(400,"Missing required field: name"),
-	CODE_401(401,"Invalid access token");
+	CODE_401(401,"Invalid access token"),
+	CODE_501(501,"Internal Server Error");
 
 	private final int code;
 	private final String message;
